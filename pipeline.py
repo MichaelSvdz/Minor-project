38096f76f154eff1c2be7948838b180ffd40db73
Duckyvite <duckyvite@gmail.com>
2017-11-16 14:55:56 +0100

    switched to sma

--- pipeline.py
+++ pipeline.py
@@ -1,11 +1,11 @@
 import matplotlib.pyplot as plt
 import ssl
-import alpha_vantage.alpha_vantage.techindicators as av
+import alpha_vantage.alpha_vantage.techindicators as ti
 
 stocks = ["MSFT", "GOOGL", "RHT"]
 for s in stocks:
-    ts = av.techindicators(key='OJGCX4Y5UT2YRRKY', output_format='pandas')
-    data, meta_data = ts.get_intraday(symbol=s,interval='1min', outputsize='full')
+    ts = ti.TechIndicators(key='OJGCX4Y5UT2YRRKY', output_format='pandas')
+    data, meta_data = ts.get_ema(symbol=s)
     data['close'].plot()
     plt.title('Intraday Times Series for the MSFT stock (1 min)')
     plt.show()
