fbd56601bb9008e18cb5e3186385a3a5405ebb40
Duckyvite <duckyvite@gmail.com>
2017-11-21 10:24:35 +0100

    Added all stocks in [market].csv

--- website/index.php
+++ website/index.php
@@ -1,1 +1,40 @@
-<?php echo "test" ?>
+<?php
+
+?>
+<script type="text/javascript">
+
+</script>
+
+<html>
+	<head>
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<title>Portfolio Chooser</title>
+		<style type="text/css">
+			@import url('https://fonts.googleapis.com/css?family=Roboto:400,700');
+			html{
+				font-family: 'Roboto', sans-serif;
+				background-color: #fdfdfc;
+			}
+			table tr td:first-child{
+				font-weight: bold;
+			}
+			button{
+				margin-bottom: 5px;
+			}
+		</style>
+	</head>
+	<body>
+		<div style="width:1100px; margin: 100 auto">
+      How long do you want to invest:
+      <input type="text" placeholder="Amount of days"><br /><br />
+      Which markets do you want?<br /><br />
+      <input type="checkbox" id="financial"><label for="financial">  Financial</label><br />
+      <input type="checkbox" id="telecommunication"><label for="telecommunication">  Telecommunication</label><br />
+		</div>
+
+		<script type="text/javascript" src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
+		<script type="text/javascript">
+
+		</script>
+	</body>
+</html>
