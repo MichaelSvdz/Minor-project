ecf602041124028bf299fd153b876a1c24c6ff1b
Duckyvite <duckyvite@gmail.com>
2017-11-21 10:39:58 +0100

    updated website with inputs

--- website/index.php
+++ website/index.php
@@ -25,11 +25,26 @@
 	</head>
 	<body>
 		<div style="width:1100px; margin: 100 auto">
+      How much money do you want to invest:
+      <input type="text" placeholder="Amount of money"><br /><br />
+
       How long do you want to invest:
       <input type="text" placeholder="Amount of days"><br /><br />
+
       Which markets do you want?<br /><br />
+      <input type="checkbox" id="consumerDiscretionary"><label for="consumerDiscretionary">  Consumer Discretionary</label><br />
+      <input type="checkbox" id="consumerStaples"><label for="consumerStaples">  Consumer Staples</label><br />
+      <input type="checkbox" id="energy"><label for="energy">  Energy</label><br />
       <input type="checkbox" id="financial"><label for="financial">  Financial</label><br />
-      <input type="checkbox" id="telecommunication"><label for="telecommunication">  Telecommunication</label><br />
+      <input type="checkbox" id="healthCare"><label for="healthCare">  Health Care</label><br />
+      <input type="checkbox" id="industrials"><label for="industrials">  Industrials</label><br />
+      <input type="checkbox" id="informationTechnology"><label for="informationTechnology">  Information Technology</label><br />
+      <input type="checkbox" id="materials"><label for="materials">  Materials</label><br />
+      <input type="checkbox" id="realEstate"><label for="realEstate">  Real Estate</label><br />
+      <input type="checkbox" id="utilities"><label for="utilities">  Utilities</label><br />
+      <br /><br />
+      <button type="submit" style="heigth:100px;width:200px">MAKE ME MILLIONAIRE</button>
+
 		</div>
 
 		<script type="text/javascript" src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
