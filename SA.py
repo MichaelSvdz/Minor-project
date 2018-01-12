bb64381974a4c0ceff5ab34b43c83f3ffa98dc40
Duckyvite <duckyvite@gmail.com>
2018-01-12 16:41:08 +0100

    made working GA

--- SA.py
+++ SA.py
@@ -3,13 +3,135 @@ import glob
 import random
 
 # Set limitations
+popSize = 10 # Size of population
+numOfGens = 10000 # Amount of generation
+chrLngth = 10 # Amount of stocks ???
 minSIP = 2 # Minimum amount of stocks in portfolio
-maxSIP = 3 # Maximum amount of stocks in portfolio
-minWeight = 0.01 # Minimum amount of weight per stocks
-maxWeight = 0.50 # Maximum amount of weight per stocks
+maxSIP = 6 # Maximum amount of stocks in portfolio
+
+# Classes
+class Chromosome:
+    def __init__(self, chrom, fitness):
+        self.chrom = chrom
+        self.fitness = fitness
 
 # Functions
+def getRandomChromosome():
+    array = [0] * chrLngth
+    c = Chromosome(array, 0)
+    stocks = random.sample(range(chrLngth), random.randint(minSIP, maxSIP))
+    for stock in stocks:
+        c.chrom[stock] = 1
+    return c
 
+def determineFitness(chrom):
+    return sum(i for i in chrom) #???
+
+def weighted_random_choice(population):
+    max = sum(p.fitness for p in population)
+    pick = random.uniform(0, max)
+    current = 0
+    for p in population:
+        current += p.fitness
+        if current > pick:
+            return p
+
+def mutate(chrom):
+    i = random.randint(0, chrLngth - 1)
+    if (chrom[i] == 0):
+        chrom[i] = 1
+    else:
+        chrom[i] = 0
+    activeStocks = sum(j for j in chrom)
+    if (activeStocks > maxSIP or activeStocks < minSIP):
+        mutate(chrom) #??? can lang duren
+
+
+# Genetic Algorithm
+# Make random population
+population = []
+for i in range(popSize):
+    population.append(getRandomChromosome())
+
+# Determine fitness
+for p in population:
+    p.fitness = determineFitness(p.chrom)
+
+for i in range(numOfGens):
+    # Make new population
+    newpopulation = []
+
+    # Elitism 2 chromosomes
+    population.sort(key=lambda x: x.fitness, reverse=True)
+    newpopulation.append(population[0])
+    newpopulation.append(population[1])
+    del population[0]
+    del population[1]
+
+    for i in range(0, popSize - 2, 2):
+        # Roulette wheel selection
+        parent1 = weighted_random_choice(population)
+        parent2 = weighted_random_choice(population)
+        while (parent1.chrom == parent2.chrom):
+            parent2 = weighted_random_choice(population)
+
+        # Crossover
+        child1 = [0] * chrLngth
+        child2 = [0] * chrLngth
+        copoint = random.randint(0, chrLngth)
+        for i in range(0, chrLngth):
+            if(i < copoint):
+                child1[i] = parent1.chrom[i]
+                child2[i] = parent2.chrom[i]
+            else:
+                child1[i] = parent2.chrom[i]
+                child2[i] = parent1.chrom[i]
+
+        newpopulation.append(Chromosome(child1, 0))
+        newpopulation.append(Chromosome(child2, 0))
+
+    population = newpopulation
+
+    # Mutation
+    mutProb = 0.005
+    for p in population:
+        if (random.uniform(0.0, 0.1) > mutProb):
+            mutate(p.chrom)
+
+    # Determine fitness
+    for p in population:
+        p.fitness = determineFitness(p.chrom)
+
+    print (sum(i.fitness for i in population))
+
+for p in population:
+    print p.chrom, p.fitness
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+'''
 # Fills empty portfolio with random solution
 def getRandomSolution():
     pf = pfClean
@@ -36,6 +158,7 @@ def getRandomSolution():
 def getFitness(pf, stockIndexes):
     return(random.uniform(0, 1))
 
+def pertubate()
 
 # Make sure the portfolio can be filled
 if (minSIP * maxWeight < 1.0):
@@ -74,7 +197,7 @@ for i in range(10):
 
     # Generate a feasible random solution, s'
     # by doing random pertubations to S*
-    pertubate()
+    pfRandom = pertubate(pfCurrent)
 
     # Compute the probability of moving, P(S <-- S')
 
@@ -92,3 +215,4 @@ for i in range(10):
 
 
 # Return the best found solution, S*
+'''
--- making_TI_data.py
+++ making_TI_data.py
@@ -1,90 +1,91 @@
 from TICalculations import TI
-from Evolution import Regression
+from Lineair_regression import Regression
 import glob
 
 myTI = TI()
 myRegression = Regression()
-number_of_samples = 15
+number_of_samples = 10
 
 dict_weights = {}
 
 datafiles =  glob.glob("datasets/*/*.csv")
-for file_name in datafiles:
-    print(file_name)
-    with open(file_name, "r") as f:
-        open_prices = []
-        high_prices = []
-        low_prices = []
-        close_prices = []
-        time_stamp = []
+#for file_name in datafiles:
+file_name = datafiles[0]
+print(file_name)
+with open(file_name, "r") as f:
+    open_prices = []
+    high_prices = []
+    low_prices = []
+    close_prices = []
+    time_stamp = []
 
-        for row in f:
-            row = row.strip()
-            row = row.split(",")
-            open_prices.append(row[1])
-            high_prices.append(row[2])
-            low_prices.append(row[3])
-            close_prices.append(row[4])
-            time_stamp.append(row[0])
+    for row in f:
+        row = row.strip()
+        row = row.split(",")
+        open_prices.append(row[1])
+        high_prices.append(row[2])
+        low_prices.append(row[3])
+        close_prices.append(row[4])
+        time_stamp.append(row[0])
 
-        del open_prices[0]
-        del high_prices[0]
-        del low_prices[0]
-        del close_prices[0]
-        del time_stamp[0]
+    del open_prices[0]
+    del high_prices[0]
+    del low_prices[0]
+    del close_prices[0]
+    del time_stamp[0]
 
-        open_prices = list(map(float, open_prices))
-        high_prices = list(map(float, high_prices))
-        low_prices = list(map(float, low_prices))
-        close_prices = list(map(float, close_prices))
+    open_prices = list(map(float, open_prices))
+    high_prices = list(map(float, high_prices))
+    low_prices = list(map(float, low_prices))
+    close_prices = list(map(float, close_prices))
 
-        SMA = []
-        EMA = []
-        GROW = []
-        MACD = []
-        STOCH = []
-        RSI = []
-        AROONUP = []
-        AROONDOWN = []
-        BBRAND = []
+    SMA = []
+    EMA = []
+    GROW = []
+    MACD = []
+    STOCH = []
+    RSI = []
+    AROONUP = []
+    AROONDOWN = []
+    BBRAND = []
 
-        for i in range(len(close_prices)-number_of_samples -1):
-            SMA.append(TI.SMA(myTI, close_prices[i:i+number_of_samples]))
+    for i in range(len(close_prices)-number_of_samples -1):
+        SMA.append(TI.SMA(myTI, close_prices[i:i+number_of_samples]))
 
-        for i in range(len(close_prices)-number_of_samples -1):
-            EMA.append(TI.EMA(myTI, close_prices[i:i+number_of_samples]))
+    for i in range(len(close_prices)-number_of_samples -1):
+        EMA.append(TI.EMA(myTI, close_prices[i:i+number_of_samples]))
 
-        for i in range(len(close_prices)-number_of_samples -1):
-            GROW.append(TI.GROW(myTI, close_prices[i:i+number_of_samples]))
+    for i in range(len(close_prices)-number_of_samples -1):
+        GROW.append(TI.GROW(myTI, close_prices[i:i+number_of_samples]))
 
-        for i in range(len(close_prices)-number_of_samples -1):
-            MACD.append(TI.MACD(myTI, close_prices[i:i+number_of_samples]))
+    for i in range(len(close_prices)-number_of_samples -1):
+        MACD.append(TI.MACD(myTI, close_prices[i:i+number_of_samples]))
 
-        for i in range(len(close_prices)-number_of_samples -1):
-            STOCH.append(TI.STOCH(myTI, low_prices[i:i+number_of_samples], high_prices[i:i+number_of_samples], close_prices[i:i+number_of_samples]))
+    for i in range(len(close_prices)-number_of_samples -1):
+        STOCH.append(TI.STOCH(myTI, low_prices[i:i+number_of_samples], high_prices[i:i+number_of_samples], close_prices[i:i+number_of_samples]))
 
-        for i in range(len(close_prices)-number_of_samples -1):
-            RSI.append(TI.RSI(myTI, close_prices[i:i+number_of_samples]))
+    for i in range(len(close_prices)-number_of_samples -1):
+        RSI.append(TI.RSI(myTI, close_prices[i:i+number_of_samples]))
 
-        for i in range(len(close_prices)-number_of_samples -1):
-            AROONUP.append(TI.AROONUP(myTI, close_prices[i:i+number_of_samples]))
+    for i in range(len(close_prices)-number_of_samples -1):
+        AROONUP.append(TI.AROONUP(myTI, close_prices[i:i+number_of_samples]))
 
-        for i in range(len(close_prices)-number_of_samples -1):
-            AROONDOWN.append(TI.AROONDOWN(myTI, close_prices[i:i+number_of_samples]))
+    for i in range(len(close_prices)-number_of_samples -1):
+        AROONDOWN.append(TI.AROONDOWN(myTI, close_prices[i:i+number_of_samples]))
 
-        for i in range(len(close_prices)-number_of_samples -1):
-            BBRAND.append(TI.BBRAND(myTI, close_prices[i:i+number_of_samples], low_prices[i:i+number_of_samples], high_prices[i:i+number_of_samples]))
+    for i in range(len(close_prices)-number_of_samples -1):
+        BBRAND.append(TI.BBRAND(myTI, close_prices[i:i+number_of_samples], low_prices[i:i+number_of_samples], high_prices[i:i+number_of_samples]))
 
-        daily_grow = []
+    daily_grow = []
 
-        for i in range(len(close_prices)-number_of_samples-2):
-            daily_grow.append(close_prices[i]-close_prices[i+1])
+    for i in range(len(close_prices)-number_of_samples-2):
+        daily_grow.append(close_prices[i]-close_prices[i+1])
 
-        training_data = []
+    training_data = []
 
-        for i in range(len(close_prices)-number_of_samples-2):
-            training_data.append([daily_grow[i], EMA[i+1]-SMA[i+1], GROW[i+1], MACD[i+1], STOCH[i+1], RSI[i+1], AROONDOWN[i+1], AROONUP[i+1]])
+    for i in range(len(close_prices)-number_of_samples-2):
+        training_data.append([daily_grow[i], EMA[i+1]-SMA[i+1], GROW[i+1], MACD[i+1], STOCH[i+1], RSI[i+1], AROONDOWN[i+1], AROONUP[i+1]])
 
-        dict_weights["{0}".format(file_name)] = Regression.regression(myRegression, training_data)
+    #dict_weights["{0}".format(file_name)] = Regression.regression(myRegression, training_data)
 
-        print(dict_weights)
+    print(Regression.regression(myRegression, training_data))
